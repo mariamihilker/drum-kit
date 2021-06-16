5a83992cd941cb9d172ac707dacdf2e9e41725cf
mariamihilker <mariamihilker@gmail.com>
2021-06-16 18:39:22 +0200

    keydown function

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-// alert("wording")
+// get all buttons as an array
 let buttonsAll = document.querySelectorAll("button");
 
-document.querySelector("button")
-
 for(let i = 0; i < buttonsAll.length; i++) {
-	buttonsAll[i].addEventListener("click", function() {
-	console.log("clicked" + " " + i);
+	buttonsAll[i].addEventListener("click", function(e) {
+	console.log("clicked" + " " + e.target.value);
+	playSound(e.target.value)
 })
+}
+
+document.addEventListener('keydown', logKey);
+
+function logKey(e) {
+  playSound(e.key)
+}
+
+
+// Call this function when clickiing buttons
+// Depends which button is clicked will be chosen different sound
+function playSound(button) {
+	let audioFile = new Audio('sounds/'+ button + '.mp3')
+	audioFile.play();
 }
